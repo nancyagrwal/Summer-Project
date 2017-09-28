b5eb5ffe145eebd9e590b4df360f00df2ecaf06f
Nancy Agrawal <nancyagrwal@gmail.com>
2017-09-28 10:27:42 -0400

    browser in ruby

--- web_browser.rb
+++ web_browser.rb
@@ -1,16 +1,14 @@
 require 'socket'
 
-host = 'www.tutorialspoint.com'			# Web server
-port = 80														# HTTP port
-path = "/index.htm"									# File we want
-
-# HTTP request to fetch a file
+host = 'www.tutorialspoint.com'			
+port = 80														
+path = "/index.htm"									
+# HTTP request 
 request = "GET #{path} HTTP/1.0\r\n\r\n"
-
-socket = TCPSocket.open(host,port)	# Connect to server
-socket.print(request)								# Send request
-response = socket.read							# Read complete response
-# Split response at first blank line into headers and body
+socket = TCPSocket.open(host,port)	
+socket.print(request)								
+response = socket.read							
+# Split response 
 headers,body = response.split("\r\n\r\n", 2)
-print body													# Display it
+print body												
 
